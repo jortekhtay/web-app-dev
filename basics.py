3782fe7a5b66c05c3483137bbad19335453cd3e7
TAY JOR TEKH <tayjortekh@gmail.com>
2023-09-28 23:09:08 +0800

    testing

--- basics.py
+++ basics.py
@@ -1,1 +1,15 @@
-print('hello world')
+#basics.py
+from flask import Flask #import flask class
+
+app = Flask(__name__) #create instance of flask app
+
+@app.route("/") #write a Python decorator
+@app.route("/hello")
+def hello():
+    #render the page
+    return "hello world!"
+
+if __name__ == '__main__':
+    # run the app server on localhost:4449
+    app.run("localhost",8080)
+
